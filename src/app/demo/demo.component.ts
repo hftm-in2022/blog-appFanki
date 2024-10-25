60de846252491b1c7e0a41630863f1eb1c14d3ca
Simon Fankhauser <simon.fankhauser@hftm.ch>
2024-10-25 15:29:23 +0200

    feat: add demo component

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,89 +1,136 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // Für ngModel
-import { MatInputModule } from '@angular/material/input'; // Für matInput
-import { MatFormFieldModule } from '@angular/material/form-field'; // Für mat-form-field
-import { MatButtonModule } from '@angular/material/button'; // Für mat-raised-button
-import { CommonModule } from '@angular/common'; // Für ngSwitch und ngClass
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-demo',
   standalone: true,
   imports: [
-    CommonModule, // Füge CommonModule hinzu
+    CommonModule,
     FormsModule,
     MatInputModule,
     MatFormFieldModule,
     MatButtonModule,
   ],
   template: `
-    <!-- Angular Material FormField with 2-way binding (NgModel) -->
-    <mat-form-field appearance="fill">
-      <mat-label>Enter your name</mat-label>
-      <input matInput [(ngModel)]="name" />
-    </mat-form-field>
+    <div class="container">
+      <!-- Eingabefeld mit Mat-Form -->
+      <mat-form-field appearance="fill" class="input-field">
+        <mat-label>Enter your name</mat-label>
+        <input matInput [(ngModel)]="name" />
+      </mat-form-field>
+      <p>Your name is: {{ name }}</p>
 
-    <p>Your name is: {{ name }}</p>
+      <!-- Benachrichtigung -->
+      <div *ngIf="submitCount > 0" class="notification">
+        Message submitted {{ submitCount }} times
+      </div>
 
-    <!-- Button with (click) event -->
-    <button mat-raised-button color="primary" (click)="onButtonClick()">
-      Submit
-    </button>
+      <!-- Button für Submit -->
+      <button
+        mat-raised-button
+        color="primary"
+        (click)="onButtonClick()"
+        class="button"
+      >
+        Submit
+      </button>
 
-    <!-- Toggle visibility with *ngIf -->
-    <button mat-raised-button color="accent" (click)="toggleVisibility()">
-      Toggle Visibility
-    </button>
-    <div *ngIf="isVisible">
-      This is conditionally visible based on isVisible!
+      <!-- Sichtbarkeit umschalten -->
+      <button
+        mat-raised-button
+        color="accent"
+        (click)="toggleVisibility()"
+        class="button"
+      >
+        Toggle Visibility
+      </button>
+      <div *ngIf="isVisible" class="visibility-box">
+        This is conditionally visible based on isVisible!
+      </div>
+
+      <!-- Farbwahl und ngSwitch -->
+      <div [ngSwitch]="color" class="color-display">
+        <p *ngSwitchCase="'red'">Red Color</p>
+        <p *ngSwitchCase="'blue'">Blue Color</p>
+        <p *ngSwitchDefault>Other Color</p>
+      </div>
+
+      <!-- Farbbuttons -->
+      <button
+        mat-raised-button
+        (click)="changeColor('red')"
+        class="button red-button"
+      >
+        Red
+      </button>
+      <button
+        mat-raised-button
+        (click)="changeColor('blue')"
+        class="button blue-button"
+      >
+        Blue
+      </button>
+      <button
+        mat-raised-button
+        (click)="changeColor('green')"
+        class="button green-button"
+      >
+        Green
+      </button>
+
+      <!-- Itemliste mit dynamischer Farbe -->
+      <ul [ngStyle]="{ color: color }" class="item-list">
+        <li *ngFor="let item of items">{{ item }}</li>
+      </ul>
+
+      <!-- Klasse umschalten -->
+      <div
+        [ngClass]="{ 'red-text': isRed, 'blue-text': !isRed }"
+        class="toggle-text"
+      >
+        This text changes class based on isRed!
+      </div>
+      <button mat-raised-button (click)="isRed = !isRed" class="button">
+        Toggle Color Class
+      </button>
+
+      <!-- Schriftgröße -->
+      <div [ngStyle]="{ 'font-size': fontSize + 'px' }" class="dynamic-text">
+        This text changes its size dynamically!
+      </div>
+      <button
+        mat-raised-button
+        (click)="fontSize = fontSize + 2"
+        class="button"
+      >
+        Increase Font Size
+      </button>
+      <button
+        mat-raised-button
+        (click)="fontSize = fontSize - 1"
+        class="button"
+      >
+        Decrease Font Size
+      </button>
     </div>
-
-    <!-- ngSwitch demonstration -->
-    <div [ngSwitch]="color">
-      <p *ngSwitchCase="'red'">Red Color</p>
-      <p *ngSwitchCase="'blue'">Blue Color</p>
-      <p *ngSwitchDefault>Other Color</p>
-    </div>
-
-    <button mat-raised-button (click)="changeColor('red')">Red</button>
-    <button mat-raised-button (click)="changeColor('blue')">Blue</button>
-    <button mat-raised-button (click)="changeColor('green')">Green</button>
-
-    <!-- ngFor demonstration -->
-    <ul>
-      <li *ngFor="let item of items">{{ item }}</li>
-    </ul>
-
-    <!-- NgClass demonstration -->
-    <div [ngClass]="{ 'red-text': isRed, 'blue-text': !isRed }">
-      This text changes class based on isRed!
-    </div>
-    <button mat-raised-button (click)="isRed = !isRed">
-      Toggle Color Class
-    </button>
-
-    <!-- NgStyle demonstration -->
-    <div [ngStyle]="{ 'font-size': fontSize + 'px' }">
-      This text changes its size dynamically!
-    </div>
-    <button mat-raised-button (click)="fontSize = fontSize + 1">
-      Increase Font Size
-    </button>
-    <button mat-raised-button (click)="fontSize = fontSize - 1">
-      Decrease Font Size
-    </button>
   `,
-  styleUrls: ['./demo.component.scss'], // Falls du Styles hast
+  styleUrls: ['./demo.component.scss'],
 })
 export class DemoComponent {
   name = '';
   isVisible = false;
-  color = 'red'; // Standardfarbe
+  color = 'red';
   items: string[] = ['Item 1', 'Item 2', 'Item 3'];
-  isRed = true; // Typannotationen sind hier nicht notwendig, wenn sie einfach sind
+  isRed = true;
   fontSize = 16;
+  submitCount = 0;
 
   onButtonClick() {
-    console.log('Button clicked!');
+    this.submitCount += 1;
   }
 
   toggleVisibility() {
