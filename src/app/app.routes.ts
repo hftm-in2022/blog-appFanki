010304399a81238bc23a772a2a4b1a5260571361
fanki <simon.fankhauser@hftm.ch>
2024-11-07 00:08:47 +0100

    feat: implement lazy loading for blog overview and detail page
    - create lazy loaded modules for blog overview and blog detail page
    - configure routes to load modules on demand to improve application performance
    - adjust module imports to support standalone components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,18 @@
 import { Routes } from '@angular/router';
-import { BlogOverviewPageComponent } from './features/blog-overview-page/blog-overview-page.component';
-import { BlogDetailsPageComponent } from './features/blog-details-page/blog-details-page.component';
 
 export const routes: Routes = [
-  { path: '', component: BlogOverviewPageComponent },
-  { path: 'blog-details/:id', component: BlogDetailsPageComponent },
+  {
+    path: '',
+    loadChildren: () =>
+      import('./features/blog-overview-page/blog-overview.module').then(
+        (m) => m.BlogOverviewModule,
+      ),
+  },
+  {
+    path: 'blog-details/:id',
+    loadChildren: () =>
+      import('./features/blog-details-page/blog-details.module').then(
+        (m) => m.BlogDetailsModule,
+      ),
+  },
 ];
