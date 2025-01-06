007af722ede575997232a5e38669fa1348d228fd
fanki <simon.fankhauser@hftm.ch>
2025-01-07 00:48:07 +0100

    fix: change dumb components

--- src/app/auth/auth.config.ts
+++ src/app/auth/auth.config.ts
@@ -1,16 +1,25 @@
-import { PassedInitialConfig } from 'angular-auth-oidc-client';
+import { provideAuth } from 'angular-auth-oidc-client';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
-export const authConfig: PassedInitialConfig = {
-  config: {
-    authority:
-      'https://d-cap-keyclaok.kindbay-711f60b2.westeurope.azurecontainerapps.io/',
-    redirectUrl: window.location.origin,
-    postLogoutRedirectUri: window.location.origin,
-    clientId: 'student@hftm.ch',
-    scope: 'openid profile email', // 'openid profile ' + your scopes
-    responseType: 'code',
-    silentRenew: true,
-    silentRenewUrl: window.location.origin + '/silent-renew.html',
-    renewTimeBeforeTokenExpiresInSeconds: 10,
+export const authProviders = [
+  provideAuth({
+    config: {
+      authority:
+        'https://d-cap-keyclaok.kindbay-711f60b2.westeurope.azurecontainerapps.io/realms/blog',
+      redirectUrl: window.location.origin,
+      postLogoutRedirectUri: window.location.origin,
+      clientId: 'spa-blog',
+      scope: 'openid profile email offline_access blogs', // Füge die benötigten Scopes hinzu
+      responseType: 'code',
+      silentRenew: true,
+      silentRenewUrl: window.location.origin + '/silent-renew.html',
+      renewTimeBeforeTokenExpiresInSeconds: 10,
+      secureRoutes: [environment.serviceUrl], // Sichere Routen definieren
+    },
+  }),
+  {
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi: true,
   },
-};
+];
